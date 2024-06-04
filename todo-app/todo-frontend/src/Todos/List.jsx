b2c25b293c438acc0958a60ff1de1c22e6ec6c6e
Marko Hakkarainen <hakkis68@gmail.com>
2024-06-04 09:57:53 +0300

    12.13

--- todo-app/todo-frontend/src/Todos/List.jsx
+++ todo-app/todo-frontend/src/Todos/List.jsx
@@ -1,49 +1,59 @@
-import React from 'react'
+// eslint-disable-next-line no-unused-vars
+import React from "react";
 
+// eslint-disable-next-line react/prop-types
 const TodoList = ({ todos, deleteTodo, completeTodo }) => {
   const onClickDelete = (todo) => () => {
-    deleteTodo(todo)
-  }
+    deleteTodo(todo);
+  };
 
   const onClickComplete = (todo) => () => {
-    completeTodo(todo)
-  }
+    completeTodo(todo);
+  };
 
   return (
     <>
-      {todos.map(todo => {
-        const doneInfo = (
-          <>
-            <span>This todo is done</span>
-            <span>
-              <button onClick={onClickDelete(todo)}> Delete </button>
-            </span>
-          </>
-        )
+      {todos
+        // eslint-disable-next-line react/prop-types
+        .map((todo) => {
+          const doneInfo = (
+            <>
+              <span>This todo is done</span>
+              <span>
+                <button onClick={onClickDelete(todo)}> Delete </button>
+              </span>
+            </>
+          );
 
-        const notDoneInfo = (
-          <>
-            <span>
-              This todo is not done
-            </span>
-            <span>
-              <button onClick={onClickDelete(todo)}> Delete </button>
-              <button onClick={onClickComplete(todo)}> Set as done </button>
-            </span>
-          </>
-        )
+          const notDoneInfo = (
+            <>
+              <span>This todo is not done</span>
+              <span>
+                <button onClick={onClickDelete(todo)}> Delete </button>
+                <button onClick={onClickComplete(todo)}> Set as done </button>
+              </span>
+            </>
+          );
 
-        return (
-          <div style={{ display: 'flex', justifyContent: 'space-between', maxWidth: '70%', margin: 'auto' }}>
-            <span>
-              {todo.text} 
-            </span>
-            {todo.done ? doneInfo : notDoneInfo}
-          </div>
-        )
-      }).reduce((acc, cur) => [...acc, <hr />, cur], [])}
+          return (
+            <div
+              key={todo.id}
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                maxWidth: "70%",
+                margin: "auto",
+              }}
+            >
+              <span>{todo.text}</span>
+              {todo.done ? doneInfo : notDoneInfo}
+            </div>
+          );
+        })
+        // eslint-disable-next-line react/jsx-key
+        .reduce((acc, cur) => [...acc, <hr />, cur], [])}
     </>
-  )
-}
+  );
+};
 
-export default TodoList
+export default TodoList;
