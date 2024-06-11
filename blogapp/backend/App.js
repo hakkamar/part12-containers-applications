318287cb048b1f346de6b5c16677a3cca44af800
Marko Hakkarainen <hakkis68@gmail.com>
2024-06-11 15:53:18 +0300

    finishing

--- blogapp/backend/App.js
+++ blogapp/backend/App.js
@@ -33,10 +33,10 @@ app.use(express.static("dist"));
 app.use(express.json());
 app.use(middleware.requestLogger);
 
-app.use("/api/blogs", blogsRouter);
-app.use("/api/users", usersRouter);
-app.use("/api/login", loginRouter);
-app.use("/api/blogs", commentsRouter);
+app.use("/blogs", blogsRouter);
+app.use("/users", usersRouter);
+app.use("/login", loginRouter);
+app.use("/blogs", commentsRouter);
 
 if (process.env.NODE_ENV === "test") {
   const testingRouter = require("./controllers/testing");
